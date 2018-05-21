e78d12ee4ad511017b37df3bf746c0486629ecc7
AesopL <wenduan@outlook.com>
2018-05-21 08:59:53 +0800

    登陆验证码

--- application/admin/validate/Login.php
+++ application/admin/validate/Login.php
@@ -8,6 +8,7 @@ class Login extends Validate
     protected $rule        =  [
         'username'         => 'require|length: 3,25',
         'password'         => 'require|length: 6,16',
+        'captcha'=>'require|captcha',
     ];
 
     protected $message     =  [
@@ -15,5 +16,7 @@ class Login extends Validate
         'username.length'  => '账号不符合',
         'password.require' => '密码不能为空',
         'password.length'  => '密码长度不符合',
+        'captcha.require'  => '验证码不能为空',
+        'captcha.captcha'  => '验证码不正确',
     ];
 }
